fa232b489374392cf27e4552c027ca9f29815ac8
Christiano <mafradesouza.cs@gmail.com>
2020-08-17 11:50:16 -0300

    contadores

---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="contadores">
+        <app-contador></app-contador>
+    </div>
+</template>
+
+<script>
+import Contador from './Contador'
+  export default{
+    components: { 'app-contador': Contador}
+  }
+</script>
+
+<style>
+
+</style>
