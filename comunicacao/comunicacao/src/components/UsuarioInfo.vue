c52d1678f1606893ae017e6d2f8891b34bca68a9
Christiano <mafradesouza.cs@gmail.com>
2020-10-23 16:43:46 -0300

    update

--- comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -2,20 +2,48 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome do usuário: <strong>{{ nome }}</strong></p>
+        <p>Nome do usuário: <strong>{{ inverterNome() }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn">Reiniciar Nome (Callback)</button>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['nome']
-}
+	props: {
+		nome: {
+			String,
+            //required: true,
+            default: "anonimo"
+        },
+        reiniciarFn: Function
+	},
+	data() {
+		return {};
+	},
+	methods: {
+		inverterNome() {
+			return this.nome
+				.split("")
+				.reverse()
+				.join("");
+        },
+        reiniciarNome(){
+            const antigo = this.nome
+            this.nome = 'Pedro';
+            this.$emit('nomeMudou', {
+                novo: this.nome,
+                antigo
+            });
+        }
+	},
+};
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.componente {
+	flex: 1;
+	background-color: #ec485f;
+	color: #fff;
+}
 </style>
